c88b23854a2732b6a9aa8fd3f898f4a2fa8aa0d6
tomas.mencner <mencnert@gmail.com>
2020-11-08 18:07:33 +0100

    React Router setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+//TODO: create HatsPage in pages
+const HatsPage = () => {
+  return <div>Hats page</div>;
+};
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path={"/"} component={HomePage} />
+        <Route exact path={"/shop/hats"} component={HatsPage} />
+      </Switch>
     </div>
   );
 }
