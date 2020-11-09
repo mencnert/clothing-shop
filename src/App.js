44ccd8f803abc59de7f2d9e37e789d6647e28329
tomas.mencner <mencnert@gmail.com>
2020-11-09 14:56:17 +0100

    shop preview page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-import "./App.css";
-import HomePage from "./pages/homepage/homepage.component";
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 
-//TODO: create HatsPage in pages
-const HatsPage = () => {
-  return <div>Hats page</div>;
-};
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path={"/"} component={HomePage} />
-        <Route exact path={"/shop/hats"} component={HatsPage} />
+        <Route path={"/shop"} component={ShopPage} />
       </Switch>
     </div>
   );
